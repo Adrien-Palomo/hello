58ff9924c7c6b05730615051296910362f690241
Adrien Palomo <apalomo3@huskers.unl.edu>
2022-08-24 22:24:59 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argv) {
+    printf("Hello, I'm Chris Bourke!\n");
+    printf("Major is Computer Science!\n");
+
+    return 0;
+}
