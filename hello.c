a532ebaac38ecedeef7c40cfc136fbfec6ec6980
Adrien Palomo <apalomo3@huskers.unl.edu>
2022-08-25 01:47:50 +0000

    Thu, Aug 25, 2022, 1:47 AM +00:00

--- hello.c
+++ hello.c
@@ -2,10 +2,9 @@
  * Author: Adrien Palomo
  * Email: apalomo3@huskers.unl.edu
  * Date: 2022/08/24
- * 
+ *
  * Basic Hello World Program
  */
-
 int main(int argc, char **argv) {
     printf("Hello, I'm Adrien Palomo!\n");
     printf("Major is Computer Science!\n");
