fd256dc63e084e863b16c0c627c28ef135436b62
Adrien Palomo <apalomo3@huskers.unl.edu>
2022-08-25 01:11:16 +0000

    Thu, Aug 25, 2022, 1:11 AM +00:00

--- hello.c
+++ hello.c
@@ -3,8 +3,9 @@
  */
 
 int main(int argc, char **argv) {
-    printf("Hello, I'm Chris Bourke!\n");
+    printf("Hello, I'm Adrien Palomo!\n");
     printf("Major is Computer Science!\n");
+    printf("Minor is Mathematics\n");
 
     return 0;
 }
